58e51dd257ccd82d588f4c9b4a5d963ccecdce41
Roland Huß <roland@jolokia.org>
2010-11-06 16:28:55 +0100

    Cleaned up license comments

--- agent/osgi-bundle/src/main/java/org/jolokia/osgi/bundle/Activator.java
+++ agent/osgi-bundle/src/main/java/org/jolokia/osgi/bundle/Activator.java
@@ -21,30 +21,6 @@ import org.ops4j.pax.web.service.jetty.internal.CompositeActivator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-/*
- * osgish - An OSGi Shell
- *
- * Copyright (C) 2009 Roland Huß, roland@cpan.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * A commercial license is available as well. Please contact roland@cpan.org for
- * further details.
- */
-
-
 /**
  * Activator for activation the embedded jolokia agent as well
  * as the Aries JMX bundle. So it's an aggregat activator.
