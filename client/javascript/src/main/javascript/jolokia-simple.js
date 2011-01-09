47dbaff7c182f73b47d6396305f70a69c8f07bf2
Roland Huß <roland@jolokia.org>
2011-01-09 17:51:54 +0100

    Worked on simple interface + unit tests

--- client/javascript/src/main/javascript/jolokia-simple.js
+++ client/javascript/src/main/javascript/jolokia-simple.js
@@ -19,34 +19,88 @@ if (Jolokia) {
         /**
          * Get one or more attributes
          *
-         * @param mbean objectname of MBean to query. Can be a pattern
-         * @param attribute attribute name. If an array, multiple attributes are fetched
-         * @param path optional path within the return value
+         * @param mbean objectname of MBean to query. Can be a pattern.
+         * @param attribute attribute name. If an array, multiple attributes are fetched.
+         *                  If <code>null</code>, all attributes are fetched.
+         * @param path optional path within the return value. For multi-attribute fetch, the path
+         *             is ignored.
          * @param opts options passed to Jolokia.request()
+         * @return the value of the attribute, possibly a complex object
          */
         function getAttribute(mbean,attribute,path,opts) {
             if (arguments.length === 3 && typeof path == "object") {
                 opts = path;
+                path = null;
+            } else if (arguments.length == 2 && typeof attribute == "object") {
+                opts = attribute;
+                attribute = null;
                 path = null;
             }
             var req = { type: "read", mbean: mbean, attribute: attribute };
             if (path != null) {
                 req.path = path;
             }
-
             return extractValue(this.request(req,prepareSucessCallback(opts)));
         }
 
+        /**
+         * Set an attribute on a MBean.
+         *
+         * @param mbean objectname of MBean to set
+         * @param attribute the attribute to set
+         * @param value the value to set
+         * @param path an optional <em>inner path</em> which, when given, is used to determine
+         *        an inner object to set the value on
+         * @param opts additional options passed to Jolokia.request()
+         * @return the previous value
+         */
+        function setAttribute(mbean,attribute,value,path,opts) {
+            if (arguments.length === 4 && typeof path == "object") {
+                opts = path;
+                path = null;
+            }
+            var req = { type: "write", mbean: mbean, attribute: attribute, value: value };
+            if (path != null) {
+                req.path = path;
+            }
+            return extractValue(this.request(req,prepareSucessCallback(opts)));
+        }
+
+        /**
+         * Execute a JMX operation and return the result value
+         *  
+         * @param mbean objectname of the MBean to operate on
+         * @param operation name of operation to execute. Can contain a signature in case overloaded
+         *                  operations are to be called (comma separated fully qualified argument types 
+         *                  append to the operation name within parentheses)
+         * @param opts optional options for Jolokia.request()
+         * @param arg1, arg2, ..... one or more argument required for executing the operation.
+         * @return the return value of the JMX operation.
+         */
+        function execute(mbean,operation,opts) {
+            var req = { type: "exec", mbean: mbean, operation: operation };
+            var start = opts == null || typeof opts != "object" ? 2 : 3;
+            if (arguments.length > start) {
+                var args = [];
+                for (var i = start; i < arguments.length; i++) {
+                    args[i-start] = arguments[i];
+                }
+                req.arguments = args;
+            }
+            return extractValue(this.request(req,prepareSucessCallback(opts)));
+        }
+        
         // =======================================================================
 
         function extractValue(response) {
             return response != null ? response.value : null;
         }
 
+        // Prepare callback to receive directly the value (instead of the full blown response)
         function prepareSucessCallback(opts) {
             if (opts && opts.success) {
                 var parm = $.extend({},opts);
-                parm.success = new function(resp) {
+                parm.success = function(resp) {
                     opts.success(resp.value);
                 };
                 return parm;
@@ -55,11 +109,12 @@ if (Jolokia) {
             }
         }
 
-
         // Extend the Jolokia prototype with new functionality (mixin)
         $.extend(Jolokia.prototype,
                  {
-                     "getAttribute" : getAttribute
+                     "getAttribute" : getAttribute,
+                     "setAttribute" : setAttribute,
+                     "execute": execute
                  });
     })(jQuery);
 } else {
