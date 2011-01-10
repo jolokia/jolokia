fa0103d8ca306f0fb0332788f0a51007be016910
Roland Huß <roland@jolokia.org>
2011-01-10 10:01:08 +0100

    Development script.

---
+++ client/javascript/test-app/makeLinks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+# Create symbolic link for fast unit test development
+# Workflow:
+# mvn clean install
+# sh makeLinks.sh
+# mvn jetty:war-exploded
+
+cd target/
+dir=`ls -d jolokia-client* | grep -v .war`
+cd $dir;
+rm jolokia-test.html
+ln -s ../../src/main/webapp/jolokia-test.html .
+rm jolokia-simple-test.html
+ln -s ../../src/main/webapp/jolokia-simple-test.html .
+cd scripts/lib
+rm jolokia.js
+ln -s ../../../../../src/main/javascript/jolokia.js .
+rm jolokia-simple.js
+ln -s ../../../../../src/main/javascript/jolokia-simple.js .
+cd ../../../..
