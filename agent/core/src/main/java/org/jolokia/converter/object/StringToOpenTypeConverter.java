9aa147f18dc7bec4a8c90138c71dadc65d9cdb1f
Roland Huß <roland@jolokia.org>
2011-09-28 10:53:38 +0200

    Refactored StringToOpenTypeConverter in order to reduce complexity.

--- agent/core/src/main/java/org/jolokia/converter/object/StringToOpenTypeConverter.java
+++ agent/core/src/main/java/org/jolokia/converter/object/StringToOpenTypeConverter.java
@@ -33,7 +33,9 @@ import org.json.simple.parser.ParseException;
  * @author Assaf Berg, roland
  * @since 02.08.11
  */
-public class StringToOpenTypeConverter {
+public class StringToOpenTypeConverter extends OpenTypeConverter {
+
+    List<OpenTypeConverter> converters;
 
     private StringToObjectConverter stringToObjectConverter;
 
@@ -43,228 +45,50 @@ public class StringToOpenTypeConverter {
      * @param pStringToObjectConverter converter for the 'leaf' values.
      */
     public StringToOpenTypeConverter(StringToObjectConverter pStringToObjectConverter) {
+        super(null);
+        converters = Arrays.asList(
+                new SimpleTypeConverter(this,pStringToObjectConverter),
+                new ArrayTypeConverter(this),
+                new CompositeTypeConverter(this),
+                new TabularDataConverter(this)
+                                  );
         stringToObjectConverter = pStringToObjectConverter;
     }
 
     /**
      * Handle conversion for OpenTypes. The value is expected to be in JSON (either
-     * an {@link JSONAware} object or its string representation
+     * an {@link JSONAware} object or its string representation.
      *
      * @param openType target type
      * @param pValue value to convert from
-     * @return the value converted
+     * @return the converted value
      */
     @SuppressWarnings("unchecked")
+    @Override
 	public Object convertToObject(OpenType openType, Object pValue) {
         if (pValue == null) {
             return null;
-        } else if (openType instanceof SimpleType) {
-            // SimpleTypes are converted as usual objects
-            return stringToObjectConverter.prepareValue(openType.getClassName(), pValue);
-		} else if (openType instanceof ArrayType<?>) {
-            return convertArrayType((ArrayType) openType, toJSON(pValue));
-        } else if (openType instanceof CompositeType) {
-            return convertCompositeType((CompositeType) openType, toJSON(pValue));
-        } else if (openType instanceof TabularType) {
-            return convertToTabularType((TabularType) openType, toJSON(pValue));
-		} else {
+        } else {
+            for (OpenTypeConverter converter : converters) {
+                if (converter.canConvert(openType)) {
+                    return converter.convertToObject(openType,pValue);
+                }
+            }
             throw new IllegalArgumentException(
                     "Cannot convert " + pValue + " to " + openType + ": " + "No converter could be found");
         }
 	}
 
-    // =======================================================================================================
-
-    private Object[] convertArrayType(ArrayType pType, JSONAware pValue) {
-        // prepare each value in the array and then process the array of values
-        if (!(pValue instanceof JSONArray)) {
-            throw new IllegalArgumentException(
-                    "Can not convert " + pValue + " to type " +
-                    pType + " because JSON object type " + pValue.getClass() + " is not a JSONArray");
-
-        }
-
-        JSONArray jsonArray = (JSONArray) pValue;
-        OpenType elementOpenType = pType.getElementOpenType();
-        Object[] valueArray = createTargetArray(elementOpenType, jsonArray.size());
-
-        int i = 0;
-        for (Object element : jsonArray) {
-            valueArray[i++] = convertToObject(elementOpenType, element);
-        }
-
-        return valueArray;
+    /**
+     * This converter is the parent converter can hence can convert
+     * all open types
+     * @param pType type (ignored)
+     * @return always true
+     */
+    @Override
+    boolean canConvert(OpenType pType) {
+        return true;
     }
 
-    // -----
-
-    private CompositeData convertCompositeType(CompositeType pType, JSONAware pValue) {
-        // break down the composite type to its field and recurse for converting each field
-        if (!(pValue instanceof JSONObject)) {
-            throw new IllegalArgumentException(
-                    "Conversion of " + pValue + " to " +
-                    pType + " failed because provided JSON type " + pValue.getClass() + " is not a JSONObject");
-        }
-
-        Map<String, Object> givenValues = (JSONObject) pValue;
-        Map<String, Object> compositeValues = new HashMap<String, Object>();
-
-        fillCompositeWithGivenValues(pType, compositeValues, givenValues);
-        completeCompositeValuesWithDefaults(pType, compositeValues);
-
-        try {
-            return new CompositeDataSupport(pType, compositeValues);
-        } catch (OpenDataException e) {
-            throw new IllegalArgumentException("Internal error: " + e.getMessage(),e);
-        }
-    }
-
-    private void fillCompositeWithGivenValues(CompositeType pType, Map<String, Object> pCompositeValues, Map<String, Object> pSourceJson) {
-        for (Map.Entry<String,Object> entry : pSourceJson.entrySet()) {
-            String key = entry.getKey();
-            Object value = entry.getValue();
-
-            if (!pType.containsKey(key)) {
-                throw new IllegalArgumentException(
-                        "Conversion to CompositeType failed because " + key + " is not known as composite attribute key.");
-            }
-            if (value != null) {
-                Object convertedValue = convertToObject(pType.getType(key),value);
-                pCompositeValues.put(key, convertedValue);
-            }
-        }
-    }
-
-    private void completeCompositeValuesWithDefaults(CompositeType pType, Map<String, Object> pCompositeValues) {
-        /* fields that were not given in the JSON must be added with
-         * null for Objects and the default value for primitives
-         */
-        for (String itemName : pType.keySet()) {
-            if (!pCompositeValues.containsKey(itemName)) {
-                Object itemValue = null;
-                OpenType itemType = pType.getType(itemName);
-                if (itemType instanceof SimpleType) {
-                    SimpleType sType = (SimpleType) itemType;
-                    itemValue = DEFAULT_PRIMITIVE_VALUES.get(sType.getClassName());
-                }
-                pCompositeValues.put(itemName, itemValue);
-            }
-        }
-    }
-
-    // -----
-
-    private TabularData convertToTabularType(TabularType pType, JSONAware pValue) {
-        CompositeType rowType = pType.getRowType();
-        if (rowType.containsKey("key") && rowType.containsKey("value") && rowType.keySet().size() == 2) {
-            return convertToTabularTypeFromMap(pType, pValue, rowType);
-        }
-
-        // =====================================================================================
-        // Its a plain TabularData, which is converted from a maps of maps
-
-        TabularDataSupport tabularData = new TabularDataSupport(pType);
-        if (!(pValue instanceof JSONObject)) {
-            throw new IllegalArgumentException("Expected JSON type for a TabularData is JSONObject, not " + pValue.getClass());
-        }
-        putRowsToTabularData(tabularData, (JSONObject) pValue, pType.getIndexNames().size());
-
-        return tabularData;
-    }
-
-    private void putRowsToTabularData(TabularDataSupport pTabularData, JSONObject pValue, int pLevel) {
-        TabularType type = pTabularData.getTabularType();
-        for (Object value : pValue.values()) {
-            if (!(value instanceof JSONObject)) {
-                throw new IllegalArgumentException(
-                        "Cannot convert " + pValue + " to type " +
-                        type + " because the object values provided (" + value.getClass() + ") is not of the expected type JSONObject at level " + pLevel);
-            }
-            JSONObject jsonValue = (JSONObject) value;
-            if (pLevel > 1) {
-                putRowsToTabularData(pTabularData, jsonValue, pLevel - 1);
-            } else {
-                pTabularData.put(convertCompositeType(type.getRowType(),jsonValue));
-            }
-        }
-    }
-
-    private TabularData convertToTabularTypeFromMap(TabularType pType, JSONAware pValue, CompositeType pRowType) {
-        // A TabularData is requested for mapping a map for the call to an MXBean
-        // as described in http://download.oracle.com/javase/6/docs/api/javax/management/MXBean.html
-        // This means, we will convert a JSONObject to the required format
-        TabularDataSupport tabularData = new TabularDataSupport(pType);
-        if (!(pValue instanceof JSONObject)) {
-            throw new IllegalArgumentException(
-                    "Cannot convert " + pValue + " to a TabularData type for an MXBean's map representation. " +
-                    "This must be a JSONObject / Map" );
-
-        }
-        @SuppressWarnings("unchecked")
-        Map<String, String> jsonObj = (Map<String,String>) pValue;
-        for(Map.Entry<String, String> entry : jsonObj.entrySet()) {
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("key", convertToObject(pRowType.getType("key"),entry.getKey()));
-            map.put("value", convertToObject(pRowType.getType("value"), entry.getValue()));
-
-            try {
-                CompositeData compositeData = new CompositeDataSupport(pRowType, map);
-                tabularData.put(compositeData);
-            } catch (OpenDataException e) {
-                throw new IllegalArgumentException(e.getMessage(),e);
-            }
-        }
-
-        return tabularData;
-    }
-
-    // ------- 
-
-    private Object[] createTargetArray(OpenType pElementType, int pLength) {
-        if (pElementType instanceof SimpleType) {
-            try {
-                SimpleType simpleType = (SimpleType) pElementType;
-			    Class elementClass = Class.forName(simpleType.getClassName());
-                return (Object[]) Array.newInstance(elementClass, pLength);
-
-            } catch (ClassNotFoundException e) {
-                throw new IllegalArgumentException("Can't find class " + pElementType.getClassName() +
-                                                   " for instantiating array: " + e.getMessage(),e);
-            }
-        } else if (pElementType instanceof CompositeType) {
-            return new CompositeData[pLength];
-		} else {
-			throw new UnsupportedOperationException("Unsupported array element type: " + pElementType);
-		}
-    }
-
-    private JSONAware toJSON(Object pValue) {
-        Class givenClass = pValue.getClass();
-        if (JSONAware.class.isAssignableFrom(givenClass)) {
-            return (JSONAware) pValue;
-        } else {
-            try {
-                return (JSONAware) new JSONParser().parse(pValue.toString());
-            } catch (ParseException e) {
-                throw new IllegalArgumentException("Cannot parse JSON " + pValue + ": " + e,e);
-            } catch (ClassCastException exp) {
-                throw new IllegalArgumentException("Given value " + pValue.toString() +
-                                                   " cannot be parsed to JSONAware object: " + exp,exp);
-            }
-        }
-    }
-
-    private static final Map<String, Object> DEFAULT_PRIMITIVE_VALUES = new HashMap<String, Object>();;
-
-    static {
-        DEFAULT_PRIMITIVE_VALUES.put(Boolean.class.getName(), false);
-        DEFAULT_PRIMITIVE_VALUES.put(Byte.class.getName(), 0);
-        DEFAULT_PRIMITIVE_VALUES.put(Character.class.getName(),'\u0000');
-        DEFAULT_PRIMITIVE_VALUES.put(Short.class.getName(), 0);
-        DEFAULT_PRIMITIVE_VALUES.put(Integer.class.getName(), 0);
-        DEFAULT_PRIMITIVE_VALUES.put(Long.class.getName(), 0L);
-        DEFAULT_PRIMITIVE_VALUES.put(Float.class.getName(), 0.0f);
-        DEFAULT_PRIMITIVE_VALUES.put(Double.class.getName(), 0.0d);
-    }
 
 }
---
+++ agent/core/src/main/java/org/jolokia/converter/object/TabularDataConverter.java
@@ -0,0 +1,117 @@
+package org.jolokia.converter.object;
+
+/*
+ * Copyright 2009-2011 Roland Huss
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.openmbean.*;
+
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+
+/**
+ * Converter for {@link TabularData}
+ *
+ * @author roland
+ * @since 28.09.11
+ */
+class TabularDataConverter extends OpenTypeConverter {
+
+    /**
+     * Constructor
+     *
+     * @param pDispatcher parent converter used for recursive conversion
+     */
+    public TabularDataConverter(OpenTypeConverter pDispatcher) {
+        super(pDispatcher);
+    }
+
+    @Override
+    boolean canConvert(OpenType pType) {
+        return pType instanceof TabularType;
+    }
+
+    @Override
+    Object convertToObject(OpenType pOpenType, Object pFrom) {
+        TabularType pType = (TabularType) pOpenType;
+        JSONAware pValue = toJSON(pFrom);
+        CompositeType rowType = pType.getRowType();
+        if (rowType.containsKey("key") && rowType.containsKey("value") && rowType.keySet().size() == 2) {
+            return convertToTabularTypeFromMap(pType, pValue, rowType);
+        }
+
+        // =====================================================================================
+        // Its a plain TabularData, which is converted from a maps of maps
+
+        TabularDataSupport tabularData = new TabularDataSupport(pType);
+        if (!(pValue instanceof JSONObject)) {
+            throw new IllegalArgumentException("Expected JSON type for a TabularData is JSONObject, not " + pValue.getClass());
+        }
+        putRowsToTabularData(tabularData, (JSONObject) pValue, pType.getIndexNames().size());
+
+        return tabularData;
+    }
+
+    // =========================================================================================
+
+    private void putRowsToTabularData(TabularDataSupport pTabularData, JSONObject pValue, int pLevel) {
+        TabularType type = pTabularData.getTabularType();
+        for (Object value : pValue.values()) {
+            if (!(value instanceof JSONObject)) {
+                throw new IllegalArgumentException(
+                        "Cannot convert " + pValue + " to type " +
+                        type + " because the object values provided (" + value.getClass() + ") is not of the expected type JSONObject at level " + pLevel);
+            }
+            JSONObject jsonValue = (JSONObject) value;
+            if (pLevel > 1) {
+                putRowsToTabularData(pTabularData, jsonValue, pLevel - 1);
+            } else {
+                pTabularData.put((CompositeData) dispatcher.convertToObject(type.getRowType(), jsonValue));
+            }
+        }
+    }
+
+    private TabularData convertToTabularTypeFromMap(TabularType pType, JSONAware pValue, CompositeType pRowType) {
+        // A TabularData is requested for mapping a map for the call to an MXBean
+        // as described in http://download.oracle.com/javase/6/docs/api/javax/management/MXBean.html
+        // This means, we will convert a JSONObject to the required format
+        TabularDataSupport tabularData = new TabularDataSupport(pType);
+        if (!(pValue instanceof JSONObject)) {
+            throw new IllegalArgumentException(
+                    "Cannot convert " + pValue + " to a TabularData type for an MXBean's map representation. " +
+                    "This must be a JSONObject / Map" );
+
+        }
+        @SuppressWarnings("unchecked")
+        Map<String, String> jsonObj = (Map<String,String>) pValue;
+        for(Map.Entry<String, String> entry : jsonObj.entrySet()) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("key", dispatcher.convertToObject(pRowType.getType("key"), entry.getKey()));
+            map.put("value", dispatcher.convertToObject(pRowType.getType("value"), entry.getValue()));
+
+            try {
+                CompositeData compositeData = new CompositeDataSupport(pRowType, map);
+                tabularData.put(compositeData);
+            } catch (OpenDataException e) {
+                throw new IllegalArgumentException(e.getMessage(),e);
+            }
+        }
+
+        return tabularData;
+    }
+}
