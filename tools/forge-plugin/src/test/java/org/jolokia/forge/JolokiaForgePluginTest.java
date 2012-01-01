0b930404ec1c8c082ff36aba6c927e7d490604ad
Roland Huß <roland@jolokia.org>
2012-01-01 22:08:50 +0100

    Initial commit for a forge plugin.

---
+++ tools/forge-plugin/src/test/java/org/jolokia/forge/JolokiaForgePluginTest.java
@@ -0,0 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jolokia.forge;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.forge.test.AbstractShellTest;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+
+public class JolokiaForgePluginTest extends AbstractShellTest
+{
+   @Deployment
+   public static JavaArchive getDeployment()
+   {
+      return AbstractShellTest.getDeployment()
+               .addPackages(true, JolokiaForgePlugin.class.getPackage());
+   }
+
+   @Test
+   public void testDefaultCommand() throws Exception
+   {
+   //   getShell().execute("jolokia");
+   }
+}
