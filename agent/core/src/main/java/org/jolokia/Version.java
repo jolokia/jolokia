5e692f2b9bfe016955c0bc5b0fabbc0bc7318614
Roland Huß <roland@jolokia.org>
2012-01-06 18:15:25 +0100

    prepared for 1.0.2

--- agent/core/src/main/java/org/jolokia/Version.java
+++ agent/core/src/main/java/org/jolokia/Version.java
@@ -25,7 +25,7 @@ package org.jolokia;
  */
 public final class Version {
 
-    private static final String VERSION = "1.0.2-SNAPSHOT";
+    private static final String VERSION = "1.0.2";
 
     // Major.Minor version of protocol
     private static final String PROTOCOL = "6.1";
