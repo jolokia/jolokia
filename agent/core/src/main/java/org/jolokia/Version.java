a91904ec5f4b268bfee03a09a35dc196e9d1fed3
Roland Huß <roland@jolokia.org>
2012-02-09 14:11:16 +0100

    Fixed issue with GlassfishDetector and regexps.

--- agent/core/src/main/java/org/jolokia/Version.java
+++ agent/core/src/main/java/org/jolokia/Version.java
@@ -25,7 +25,7 @@ package org.jolokia;
  */
 public final class Version {
 
-    private static final String VERSION = "1.0.2";
+    private static final String VERSION = "1.0.3-SNAPSHOT";
 
     // Major.Minor version of protocol
     private static final String PROTOCOL = "6.1";
