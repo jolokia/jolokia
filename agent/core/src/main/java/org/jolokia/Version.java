f5509ab0ad347c5a3f58f6939968a3eb1fd08672
Roland Huß <roland@jolokia.org>
2013-01-22 20:36:55 +0100

    Bumped up version number to 1.1.0-SNAPSHOT

--- agent/core/src/main/java/org/jolokia/Version.java
+++ agent/core/src/main/java/org/jolokia/Version.java
@@ -25,7 +25,7 @@ package org.jolokia;
  */
 public final class Version {
 
-    private static final String VERSION = "1.0.7-SNAPSHOT";
+    private static final String VERSION = "1.1.0-SNAPSHOT";
 
     // Major.Minor version of protocol
     private static final String PROTOCOL = "7.0";
