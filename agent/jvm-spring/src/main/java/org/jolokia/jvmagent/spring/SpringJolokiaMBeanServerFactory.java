00ef5cc78b7af8004bb020483650e968b92d15a2
Roland Huß <roland@jolokia.org>
2013-02-12 11:39:51 +0100

    Added missing files.

---
+++ agent/jvm-spring/src/main/java/org/jolokia/jvmagent/spring/SpringJolokiaMBeanServerFactory.java
@@ -0,0 +1,43 @@
+package org.jolokia.jvmagent.spring;
+
+/*
+ * Copyright 2009-2013 Roland Huss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.management.MBeanServer;
+
+import org.jolokia.jmx.JolokiaMBeanServerUtil;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * Factory bean for looking up the jolokia MBeanServer
+ *
+ * @author roland
+ * @since 11.02.13
+ */
+public class SpringJolokiaMBeanServerFactory implements FactoryBean<MBeanServer> {
+
+    public MBeanServer getObject() throws Exception {
+        return JolokiaMBeanServerUtil.getJolokiaMBeanServer();
+    }
+
+    public Class<?> getObjectType() {
+        return MBeanServer.class;
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+}
