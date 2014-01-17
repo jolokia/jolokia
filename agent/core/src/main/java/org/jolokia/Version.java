994a948176a088408bda860cfafd9a1015ac9af8
Roland Huß <roland@jolokia.org>
2014-01-17 11:58:27 +0100

    Increased finally the version number

--- agent/core/src/main/java/org/jolokia/Version.java
+++ agent/core/src/main/java/org/jolokia/Version.java
@@ -25,7 +25,7 @@ package org.jolokia;
  */
 public final class Version {
 
-    private static final String VERSION = "1.2.0";
+    private static final String VERSION = "2.0-SNAPSHOT";
 
     // Major.Minor version of protocol
     private static final String PROTOCOL = "7.1";
