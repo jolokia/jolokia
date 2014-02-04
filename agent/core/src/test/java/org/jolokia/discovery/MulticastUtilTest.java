2dace8fb4ac1a2b48eaadf36bb29deaebb6f8393
Roland Huß <roland@jolokia.org>
2014-02-04 09:42:33 +0100

    Fixed test

--- agent/core/src/test/java/org/jolokia/discovery/MulticastUtilTest.java
+++ agent/core/src/test/java/org/jolokia/discovery/MulticastUtilTest.java
@@ -16,7 +16,7 @@ public class MulticastUtilTest {
 
     @Test
     public void createSocketNoAddress() throws IOException {
-        InetAddress address = Inet6Address.getByName("fe80::e2f8:47ff:fe42:d872%en1");
+        InetAddress address = Inet6Address.getByName("fe80::e2f8:47ff:fe42:d872");
         MulticastUtil.newMulticastSocket(address);
     }
 
