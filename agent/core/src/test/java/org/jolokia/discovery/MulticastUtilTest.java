7d0415521045354706081f308e676cffe9f87b56
Roland Huß <roland@jolokia.org>
2014-04-01 07:43:39 +0200

    Fixed tests, failure introduced on travis by fixes for #135

--- agent/core/src/test/java/org/jolokia/discovery/MulticastUtilTest.java
+++ agent/core/src/test/java/org/jolokia/discovery/MulticastUtilTest.java
@@ -5,7 +5,10 @@ import java.net.Inet6Address;
 import java.net.InetAddress;
 
 import org.jolokia.util.LogHandler;
+import org.jolokia.util.NetworkUtil;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.fail;
 
 /**
  * @author roland
@@ -16,7 +19,15 @@ public class MulticastUtilTest {
     @Test
     public void createSocketNoAddress() throws IOException {
         InetAddress address = Inet6Address.getByName("fe80::e2f8:47ff:fe42:d872");
-        MulticastUtil.newMulticastSocket(address, LogHandler.QUIET);
+        if (NetworkUtil.isMulticastSupported()) {
+            MulticastUtil.newMulticastSocket(address, LogHandler.QUIET);
+        } else {
+            try {
+                MulticastUtil.newMulticastSocket(address, LogHandler.QUIET);
+                fail();
+            } catch (IOException exp) {
+                // Expected since no multicast socket is available
+            }
+        }
     }
-
 }
