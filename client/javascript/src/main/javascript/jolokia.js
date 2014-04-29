66331c532e08cf25144c4c43473c729cbd4a463a
Roland Huß <roland@jolokia.org>
2014-04-29 23:24:52 +0200

    Merged in 1.2.1

--- client/javascript/src/main/javascript/jolokia.js
+++ client/javascript/src/main/javascript/jolokia.js
@@ -66,7 +66,7 @@
             }
 
             // Jolokia Javascript Client version
-            this.CLIENT_VERSION = "1.2.0";
+            this.CLIENT_VERSION = "1.2.1";
 
             // Registered requests for fetching periodically
             var jobs = [];
