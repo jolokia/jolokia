a32934188316d67f07c4992f48d98e831b1eeeee
Roland Huß <roland@jolokia.org>
2015-05-28 09:05:56 +0200

    Update version number to 1.3.1

--- client/javascript/src/main/javascript/jolokia.js
+++ client/javascript/src/main/javascript/jolokia.js
@@ -66,7 +66,7 @@
             }
 
             // Jolokia Javascript Client version
-            this.CLIENT_VERSION = "1.3.0";
+            this.CLIENT_VERSION = "1.3.1";
 
             // Registered requests for fetching periodically
             var jobs = [];
