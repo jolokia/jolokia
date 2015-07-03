db5b26e3ec9ab451fcb1bc6e9d6e71d507118533
Roland Huß <roland@jolokia.org>
2015-07-03 20:27:44 +0200

    Update version

--- agent/core/src/main/java/org/jolokia/Version.java
+++ agent/core/src/main/java/org/jolokia/Version.java
@@ -25,7 +25,7 @@ package org.jolokia;
  */
 public final class Version {
 
-    private static final String VERSION = "1.3.1";
+    private static final String VERSION = "1.3.2-SNAPSHOT";
 
     // Major.Minor version of protocol
     private static final String PROTOCOL = "7.2";
