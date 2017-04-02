e1c969fdcace8526dba876a81ad4df7e785d6a09
Roland Huß <roland@jolokia.org>
2017-04-02 23:11:29 +0200

    Preparation for 1.3.6

--- client/javascript/src/main/javascript/jolokia.js
+++ client/javascript/src/main/javascript/jolokia.js
@@ -66,7 +66,7 @@
             }
 
             // Jolokia Javascript Client version
-            this.CLIENT_VERSION = "1.3.5";
+            this.CLIENT_VERSION = "1.3.6";
 
             // Registered requests for fetching periodically
             var jobs = [];
