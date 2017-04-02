5cc1582031ced96634b1759b2d3e357d3c4142bd
Roland Huß <roland@jolokia.org>
2017-04-02 18:29:24 +0200

    Set version to 1.3.6 snapshot

--- agent/core/src/main/java/org/jolokia/Version.java
+++ agent/core/src/main/java/org/jolokia/Version.java
@@ -25,7 +25,7 @@ package org.jolokia;
  */
 public final class Version {
 
-    private static final String VERSION = "1.3.5";
+    private static final String VERSION = "1.3.6-SNAPSHOT";
 
     // Major.Minor version of protocol
     private static final String PROTOCOL = "7.2";
