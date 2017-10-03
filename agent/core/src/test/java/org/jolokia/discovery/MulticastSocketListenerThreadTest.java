94650b54d16b845d262caa004aac6e54a5a7f9f2
marska <martin.skarsaune@kantega.no>
2017-10-04 00:47:45 +0200

    Give discovery threads names to indicate ownership
    Signed-off-by: marska <martin.skarsaune@kantega.no>

--- agent/core/src/test/java/org/jolokia/discovery/MulticastSocketListenerThreadTest.java
+++ agent/core/src/test/java/org/jolokia/discovery/MulticastSocketListenerThreadTest.java
@@ -38,7 +38,7 @@ public class MulticastSocketListenerThreadTest {
         details.setSecured(false);
         details.setServerInfo("jolokia", "jolokia-test", "1.0");
 
-        MulticastSocketListenerThread listenerThread = new MulticastSocketListenerThread(null,
+        MulticastSocketListenerThread listenerThread = new MulticastSocketListenerThread("ListenerThread", null,
                                                getAgentDetailsHolder(details),
                                                new AllowAllRestrictor(),
                                                new LogHandler.StdoutLogHandler(true));
