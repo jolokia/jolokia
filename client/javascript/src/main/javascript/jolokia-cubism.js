09ba54ad701e4be834a7112c7b06efad154b54e9
Roland Huß <roland@ro14nd.de>
2018-01-23 15:56:01 +0100

    feature: Preparartions for release 1.4.0

--- client/javascript/src/main/javascript/jolokia-cubism.js
+++ client/javascript/src/main/javascript/jolokia-cubism.js
@@ -23,7 +23,7 @@
 (function () {
     var builder = function (cubism,Jolokia) {
 
-        var VERSION = "1.3.7";
+        var VERSION = "1.4.0";
 
         var ctx_jolokia = function (url, opts) {
             var source = {},
--- client/javascript/src/main/javascript/jolokia.js
+++ client/javascript/src/main/javascript/jolokia.js
@@ -66,7 +66,7 @@
             }
 
             // Jolokia Javascript Client version
-            this.CLIENT_VERSION = "1.3.7";
+            this.CLIENT_VERSION = "1.4.0";
 
             // Registered requests for fetching periodically
             var jobs = [];
