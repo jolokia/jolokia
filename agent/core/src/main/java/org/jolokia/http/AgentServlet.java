fd7b93da30c61a45bac10d8b311f1b79a74910f5
Roland Huß <roland@jolokia.org>
2018-02-08 10:13:21 +0100

    chore(war-agent): Throw exception if proxy mode is used but not enabled explicitly.
    This is to avoid confusion as it was switched on by default and if
    a proxy request would simply be ignored, then the MBeans from the
    proxy server would be silently returned without error.

--- agent/core/src/main/java/org/jolokia/http/AgentServlet.java
+++ agent/core/src/main/java/org/jolokia/http/AgentServlet.java
@@ -157,18 +157,30 @@ public class AgentServlet extends HttpServlet {
      * @param pConfig configuration to update
      */
     private void addJsr160DispatcherIfExternallyConfigured(Configuration pConfig) {
-        for (String param : new String[] {
-            System.getProperty("org.jolokia.jsr160ProxyEnabled"),
-            System.getenv("JOLOKIA_JSR160_PROXY_ENABLED")
-        }) {
-            if (param !=null && (param.isEmpty() || Boolean.parseBoolean(param))){
-                String dispatchers = pConfig.get(ConfigKey.DISPATCHER_CLASSES);
+        String dispatchers = pConfig.get(ConfigKey.DISPATCHER_CLASSES);
+        String jsr160DispatcherClass = "org.jolokia.jsr160.Jsr160RequestDispatcher";
 
-                pConfig.updateGlobalConfiguration(
-                    Collections.singletonMap(
-                        ConfigKey.DISPATCHER_CLASSES.getKeyValue(),
-                        (dispatchers != null ? dispatchers + "," : "") + "org.jolokia.jsr160.Jsr160RequestDispatcher"));
-                return;
+        if (dispatchers == null || !dispatchers.contains(jsr160DispatcherClass)) {
+            for (String param : new String[]{
+                System.getProperty("org.jolokia.jsr160ProxyEnabled"),
+                System.getenv("JOLOKIA_JSR160_PROXY_ENABLED")
+            }) {
+                if (param != null && (param.isEmpty() || Boolean.parseBoolean(param))) {
+                    {
+                        pConfig.updateGlobalConfiguration(
+                            Collections.singletonMap(
+                                ConfigKey.DISPATCHER_CLASSES.getKeyValue(),
+                                (dispatchers != null ? dispatchers + "," : "") + jsr160DispatcherClass));
+                    }
+                    return;
+                }
+            }
+            if (dispatchers == null) {
+                // We add a breaking dispatcher to avoid silently ignoring a JSR160 proxy request
+                // when it is now enabled
+                pConfig.updateGlobalConfiguration(Collections.singletonMap(
+                    ConfigKey.DISPATCHER_CLASSES.getKeyValue(),
+                    Jsr160ProxyNotEnabledByDefaultAnymoreDispatcher.class.getCanonicalName()));
             }
         }
     }
