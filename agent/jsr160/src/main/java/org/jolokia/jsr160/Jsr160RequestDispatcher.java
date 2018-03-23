f174eb88703dc141e4ac55b07a38e2631d7ad5f1
Roland Huß <roland@jolokia.org>
2018-03-23 18:38:30 +0100

    revert last change as it breaks regular remote JMX access

--- agent/jsr160/src/main/java/org/jolokia/jsr160/Jsr160RequestDispatcher.java
+++ agent/jsr160/src/main/java/org/jolokia/jsr160/Jsr160RequestDispatcher.java
@@ -244,7 +244,7 @@ public class Jsr160RequestDispatcher implements RequestDispatcher {
 
     private Set<String> extractBlackList(Configuration pConfig) {
         // Bad, bad ....
-        return Collections.singleton("service:jmx:rmi:///jndi.*");
+        return Collections.singleton("service:jmx:rmi:///jndi/ldap:.*");
     }
 
 
