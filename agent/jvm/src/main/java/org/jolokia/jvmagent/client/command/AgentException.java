fe418b712e4794251800eddcbac5f10ddc8dd7a5
David Heitzman <dheitzman@interactions.com>
2019-08-12 14:25:29 -0400

    removed copyright

--- agent/jvm/src/main/java/org/jolokia/jvmagent/client/command/AgentException.java
+++ agent/jvm/src/main/java/org/jolokia/jvmagent/client/command/AgentException.java
@@ -1,13 +1,5 @@
-// Copyright (C) 2019 Interactions Corporation
-// All rights reserved
-/*
- * Created on 2019-08-07
- */
 package org.jolokia.jvmagent.client.command;
 
-/**
- * @author dheitzman
- */
 public class AgentException extends Exception {
 
 	public AgentException(String message) {
