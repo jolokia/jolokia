5a52e4aaf8214cc04723b0c4f8924a92c76838fa
Grzegorz Grzybek <gr.grzybek@gmail.com>
2023-10-10 13:41:08 +0200

    [site] Preview for 2.0 site + reference documentation

---
+++ _2/js/flattr.js
@@ -0,0 +1,10 @@
+(function() {
+    var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+    
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'http://api.flattr.com/js/0.5.0/load.js?mode=auto';
+    
+    t.parentNode.insertBefore(s, t);
+})();
+
