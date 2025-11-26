dc870d1557072d47778d79b18fbd89b4d9929eb2
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-11-26 12:23:42 +0100

    [javascript] Prepare NPM version 2.4.1

--- client/javascript-esm/packages/jolokia/src/jolokia.ts
+++ client/javascript-esm/packages/jolokia/src/jolokia.ts
@@ -66,7 +66,7 @@ type RequestArguments = {
   fetchErrorCb?: FetchErrorCallback
 }
 
-const CLIENT_VERSION = "2.4.0"
+const CLIENT_VERSION = "2.4.1"
 
 /**
  * Default parameters for GET and POST requests
